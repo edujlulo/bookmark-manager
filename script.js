cadfa2d4809848a9e566adcb83686658fca8c965
Eduardo Lulo <eduardo.lulo.m@gmail.com>
2025-10-21 23:24:44 +0100

    Updates

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+const mainSection = document.getElementById("main-section");
+const formSection = document.getElementById("form-section");
+
 function getBookmarks() {
   let bookmarks = localStorage.getItem("bookmarks") || [];
   return bookmarks;
 }
+
+function displayOrCloseForm() {}
