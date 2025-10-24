9a851fa67a38a02c04755b19e5b7843cb8849f52
Eduardo Lulo <eduardo.lulo.m@gmail.com>
2025-10-24 21:01:04 +0100

    Updates

--- script.js
+++ script.js
@@ -7,11 +7,3 @@ function getBookmarks() {
 }
 
 function displayOrCloseForm() {}
-
-const personas2 = [
-  { nombre: "Ana", edad: 25 },
-  { nombre: "Juan", edad: 19 },
-  { nombre: "Luis", edad: 30 },
-];
-
-console.log(personas2.reduce((acc, p) => acc + p.edad, 0));
