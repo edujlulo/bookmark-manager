9c1049b1efec708ec5666da4179745d0007ccb94
Eduardo Lulo <eduardo.lulo.m@gmail.com>
2025-11-04 23:57:51 +0000

    Updates

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const mainSection = document.getElementById("main-section");
 const formSection = document.getElementById("form-section");
 const addBookmarkBtn = document.getElementById("add-bookmark-button");
+const categoryDropdown = document.getElementById("category-dropdown");
+const categoryName = document.querySelectorAll(".category-name");
 
 function getBookmarks() {
   let bookmarks = localStorage.getItem("bookmarks") || [];
